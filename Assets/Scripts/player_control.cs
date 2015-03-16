916780c21e5c40670f302e72b01953679cc187d3
gbastien1 <gabrielle.bastien1@gmail.com>
2015-03-16 09:47:14 -0400

    premier test de control script

---
+++ Assets/Scripts/player_control.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_control : MonoBehaviour {
+    
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player.transform.position = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+	}
+}
